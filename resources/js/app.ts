425f2a9a371409eb842ddd74d4f1c288f4ebd17e
teoh4770 <teoh4770@mylaurier.ca>
2026-01-07 08:54:22 -0500

    install PrimeVue
    - install PrimeVue, PrimeVue theme and its auto import resolver package
    - install related plugins to our Vue app in app.ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,13 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
+import Aura from '@primeuix/themes/aura';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import PrimeVue from 'primevue/config';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { initializeTheme } from './composables/useAppearance';
+import { definePreset } from '@primeuix/themes';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,9 +19,34 @@ createInertiaApp({
             import.meta.glob<DefineComponent>('./pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el);
+        const app = createApp({ render: () => h(App, props) });
+
+        const MyPreset = definePreset(Aura, {
+            semantic: {
+                primary: {
+                    50: '{sky.50}',
+                    100: '{sky.100}',
+                    200: '{sky.200}',
+                    300: '{sky.300}',
+                    400: '{sky.400}',
+                    500: '{sky.500}',
+                    600: '{sky.600}',
+                    700: '{sky.700}',
+                    800: '{sky.800}',
+                    900: '{sky.900}',
+                    950: '{sky.950}'
+                }
+            }
+        });
+
+        app.use(plugin);
+        app.use(PrimeVue, {
+            theme: {
+                preset: MyPreset,
+            },
+        });
+
+        app.mount(el);
     },
     progress: {
         color: '#4B5563',
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
     plugins: [
@@ -23,5 +25,10 @@ export default defineConfig({
                 },
             },
         }),
+        Components({
+            resolvers: [
+                PrimeVueResolver()
+            ]
+        })
     ],
 });
